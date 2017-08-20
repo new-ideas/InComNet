eda44c0f419be20cabfa4e4e3e764f7238cba86a
Md. Asaduzzaman <asaduzzaman.cse@gmail.com>
2017-08-20 22:20:14 +0600

    setup entrust role management

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,25 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        \DB::statement('SET FOREIGN_KEY_CHECKS=0');
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
+            $table->string('username');
             $table->string('password');
+            $table->boolean('status')->default(0)->comment('0=in-active, 1=Active, -1=Suspended'); // 1 is for active , 0 is for suspended
+            $table->dateTime('last_login')->nullable();
+            $table->string('last_login_ip')->nullable();
+            $table->tinyInteger('phone_verified')->default(0);
+            $table->tinyInteger('email_verified')->default(0);
+            $table->tinyInteger('document_verified')->default(0);
+            $table->tinyInteger('email_verify_token')->default(0);
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
+        \DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 
     /**
